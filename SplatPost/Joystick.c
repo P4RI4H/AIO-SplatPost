fcb471c14bd009828f66dfdd4dd2905ca0d223ae
P4RI4H <P4RI4H@yandex.com>
2018-04-27 23:29:43 -0400

    Add files via upload

--- SplatPost/Joystick.c
+++ SplatPost/Joystick.c
@@ -20,13 +20,58 @@ these buttons for our use.
 
 /** \file
  *
- *  Main source file for the posts printer demo. This file contains the main tasks of
- *  the demo and is responsible for the initial application hardware configuration.
+ *  Main source file for the Joystick demo. This file contains the main tasks of the demo and
+ *  is responsible for the initial application hardware configuration.
  */
 
 #include "Joystick.h"
 
-extern const uint8_t image_data[0x12c1] PROGMEM;
+/*
+The following ButtonMap variable defines all possible buttons within the
+original 13 bits of space, along with attempting to investigate the remaining
+3 bits that are 'unused'. This is what led to finding that the 'Capture'
+button was operational on the stick.
+*/
+uint16_t ButtonMap[16] = {
+	0x01, //Y
+	0x02, //B
+	0x04, //A
+	0x08, //X
+	0x10, //L
+	0x20, //R
+	0x40, //ZL
+	0x80, //ZR
+	0x100, //Minus
+	0x200, //Plus
+	0x400, //L-stick
+	0x800, //R-stick
+	0x1000, //Unk
+	0x2000, //Unk
+	0x4000, //Unk
+	0x8000, //Unk
+};
+
+uint8_t image_data[0x12c1];
+
+/*** Debounce ****
+The following is some -really bad- debounce code. I have a more robust library
+that I've used in other personal projects that would be a much better use
+here, especially considering that this is a stick indented for use with arcade
+fighters.
+
+This code exists solely to actually test on. This will eventually be replaced.
+**** Debounce ***/
+// Quick debounce hackery!
+// We're going to capture each port separately and store the contents into a 32-bit value.
+uint32_t pb_debounce = 0;
+uint32_t pd_debounce = 0;
+
+// We also need a port state capture. We'll use a 16-bit value for this.
+uint16_t bd_state = 0;
+
+// We'll also give us some useful macros here.
+#define PINB_DEBOUNCED ((bd_state >> 0) & 0xFF)
+#define PIND_DEBOUNCED ((bd_state >> 8) & 0xFF) 
 
 // Main entry point.
 int main(void) {
@@ -54,16 +99,12 @@ void SetupHardware(void) {
 	clock_prescale_set(clock_div_1);
 	// We can then initialize our hardware and peripherals, including the USB stack.
 
-	#ifdef ALERT_WHEN_DONE
-	// Both PORTD and PORTB will be used for the optional LED flashing and buzzer.
-	#warning LED and Buzzer functionality enabled. All pins on both PORTB and \
-PORTD will toggle when printing is done.
-	DDRD  = 0xFF; //Teensy uses PORTD
-	PORTD =  0x0;
-                  //We'll just flash all pins on both ports since the UNO R3
-	DDRB  = 0xFF; //uses PORTB. Micro can use either or, but both give us 2 LEDs
-	PORTB =  0x0; //The ATmega328P on the UNO will be resetting, so unplug it?
-	#endif
+	// Both PORTD and PORTB will be used for handling the buttons and stick.
+	DDRD  &= ~0xFF;
+	PORTD |=  0xFF;
+
+	DDRB  &= ~0xFF;
+	PORTB |=  0xFF;
 	// The USB stack should be initialized last.
 	USB_Init();
 }
@@ -92,15 +133,47 @@ void EVENT_USB_Device_ConfigurationChanged(void) {
 // Process control requests sent to the device from the USB host.
 void EVENT_USB_Device_ControlRequest(void) {
 	// We can handle two control requests: a GetReport and a SetReport.
+	switch (USB_ControlRequest.bRequest)
+	{
+		// GetReport is a request for data from the device.
+		case HID_REQ_GetReport:
+			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+			{
+				// We'll create an empty report.
+				USB_JoystickReport_Input_t JoystickInputData;
+				// We'll then populate this report with what we want to send to the host.
+				GetNextReport(&JoystickInputData);
+				// Since this is a control endpoint, we need to clear up the SETUP packet on this endpoint.
+				Endpoint_ClearSETUP();
+				// Once populated, we can output this data to the host. We do this by first writing the data to the control stream.
+				Endpoint_Write_Control_Stream_LE(&JoystickInputData, sizeof(JoystickInputData));
+				// We then acknowledge an OUT packet on this endpoint.
+				Endpoint_ClearOUT();
+			}
 
-	// Not used here, it looks like we don't receive control request from the Switch.
+			break;
+		case HID_REQ_SetReport:
+			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+			{
+				// We'll create a place to store our data received from the host.
+				USB_JoystickReport_Output_t JoystickOutputData;
+				// Since this is a control endpoint, we need to clear up the SETUP packet on this endpoint.
+				Endpoint_ClearSETUP();
+				// With our report available, we read data from the control stream.
+				Endpoint_Read_Control_Stream_LE(&JoystickOutputData, sizeof(JoystickOutputData));
+				// We then send an IN packet on this endpoint.
+				Endpoint_ClearIN();
+			}
+
+			break;
+	}
 }
 
 // Process and deliver data from IN and OUT endpoints.
 void HID_Task(void) {
 	// If the device isn't connected and properly configured, we can't do anything here.
 	if (USB_DeviceState != DEVICE_STATE_Configured)
-		return;
+	  return;
 
 	// We'll start with the OUT endpoint.
 	Endpoint_SelectEndpoint(JOYSTICK_OUT_EPADDR);
@@ -113,9 +186,8 @@ void HID_Task(void) {
 			// We'll create a place to store our data received from the host.
 			USB_JoystickReport_Output_t JoystickOutputData;
 			// We'll then take in that data, setting it up in our storage.
-			while(Endpoint_Read_Stream_LE(&JoystickOutputData, sizeof(JoystickOutputData), NULL) != ENDPOINT_RWSTREAM_NoError);
+			Endpoint_Read_Stream_LE(&JoystickOutputData, sizeof(JoystickOutputData), NULL);
 			// At this point, we can react to this data.
-
 			// However, since we're not doing anything with this data, we abandon it.
 		}
 		// Regardless of whether we reacted to the data, we acknowledge an OUT packet on this endpoint.
@@ -132,138 +204,158 @@ void HID_Task(void) {
 		// We'll then populate this report with what we want to send to the host.
 		GetNextReport(&JoystickInputData);
 		// Once populated, we can output this data to the host. We do this by first writing the data to the control stream.
-		while(Endpoint_Write_Stream_LE(&JoystickInputData, sizeof(JoystickInputData), NULL) != ENDPOINT_RWSTREAM_NoError);
+		Endpoint_Write_Stream_LE(&JoystickInputData, sizeof(JoystickInputData), NULL);
 		// We then send an IN packet on this endpoint.
 		Endpoint_ClearIN();
+
+		/* Clear the report data afterwards */
+		// memset(&JoystickInputData, 0, sizeof(JoystickInputData));
 	}
 }
 
-typedef enum {
-	SYNC_CONTROLLER,
-	SYNC_POSITION,
-	STOP_X,
-	STOP_Y,
-	MOVE_X,
-	MOVE_Y,
-	DONE
-} State_t;
-State_t state = SYNC_CONTROLLER;
-
-#define ECHOES 2
-int echoes = 0;
-USB_JoystickReport_Input_t last_report;
-
-int report_count = 0;
+int meme = 1;
+int input_brakes = 0;
+int input_count = 0;
 int xpos = 0;
-int ypos = 0;
-int portsval = 0;
+int ypos = -1;
+bool print_right = true;
+bool other = true;
+
+bool reset_request = false;
+bool reset = true;
+int reset_count = 0;
+
+bool done_printing = false;
 
 // Prepare the next report for the host.
 void GetNextReport(USB_JoystickReport_Input_t* const ReportData) {
+	// All of this code here is handled -really poorly-, and should be replaced with something a bit more production-worthy.
+	uint16_t buf_button   = 0x00;
+	uint8_t  buf_joystick = 0x00;
 
-	// Prepare an empty report
+	/* Clear the report contents */
 	memset(ReportData, 0, sizeof(USB_JoystickReport_Input_t));
-	ReportData->LX = STICK_CENTER;
-	ReportData->LY = STICK_CENTER;
-	ReportData->RX = STICK_CENTER;
-	ReportData->RY = STICK_CENTER;
-	ReportData->HAT = HAT_CENTER;
 
-	// Repeat ECHOES times the last report
-	if (echoes > 0)
-	{
-		memcpy(ReportData, &last_report, sizeof(USB_JoystickReport_Input_t));
-		echoes--;
-		return;
+	buf_button   = 0x0;
+	buf_joystick = 0x0;
+
+	for (int i = 0; i < 16; i++) {
+		if (buf_button & (1 << i))
+			ReportData->Button |= ButtonMap[i];
 	}
 
-	// States and moves management
-	switch (state)
-	{
-		case SYNC_CONTROLLER:
-			if (report_count > 100)
-			{
-				report_count = 0;
-				state = SYNC_POSITION;
-			}
-			else if (report_count == 25 || report_count == 50)
-			{
-				ReportData->Button |= SWITCH_L | SWITCH_R;
-			}
-			else if (report_count == 75 || report_count == 100)
-			{
-				ReportData->Button |= SWITCH_A;
-			}
-			report_count++;
+	if (buf_joystick & 0x10)
+		ReportData->LX = 0;
+	else if (buf_joystick & 0x20)
+		ReportData->LX = 255;
+	else
+		ReportData->LX = 128;
+
+	if (buf_joystick & 0x80)
+		ReportData->LY = 0;
+	else if (buf_joystick & 0x40)
+		ReportData->LY = 255;
+	else
+		ReportData->LY = 128;
+
+	switch(buf_joystick & 0xF0) {
+		case 0x80: // Top
+			ReportData->HAT = 0x00;
 			break;
-		case SYNC_POSITION:
-			if (report_count == 250)
-			{
-				report_count = 0;
-				xpos = 0;
-				ypos = 0;
-				state = STOP_X;
-			}
-			else
-			{
-				// Moving faster with LX/LY
-				ReportData->LX = STICK_MIN;
-				ReportData->LY = STICK_MIN;
-			}
-			if (report_count == 75 || report_count == 150)
-			{
-				// Clear the screen
-				ReportData->Button |= SWITCH_MINUS;
-			}
-			report_count++;
+		case 0xA0: // Top-Right
+			ReportData->HAT = 0x01;
 			break;
-		case STOP_X:
-			state = MOVE_X;
+		case 0x20: // Right
+			ReportData->HAT = 0x02;
 			break;
-		case STOP_Y:
-			if (ypos < 120 - 1)
-				state = MOVE_Y;
-			else
-				state = DONE;
+		case 0x60: // Bottom-Right
+			ReportData->HAT = 0x03;
 			break;
-		case MOVE_X:
-			if (ypos % 2)
-			{
-				ReportData->HAT = HAT_LEFT;
-				xpos--;
-			}
-			else
-			{
-				ReportData->HAT = HAT_RIGHT;
-				xpos++;
-			}
-			if (xpos > 0 && xpos < 320 - 1)
-				state = STOP_X;
-			else
-				state = STOP_Y;
+		case 0x40: // Bottom
+			ReportData->HAT = 0x04;
 			break;
-		case MOVE_Y:
-			ReportData->HAT = HAT_BOTTOM;
-			ypos++;
-			state = STOP_X;
+		case 0x50: // Bottom-Left
+			ReportData->HAT = 0x05;
 			break;
-		case DONE:
-			#ifdef ALERT_WHEN_DONE
-			portsval = ~portsval;
-			PORTD = portsval; //flash LED(s) and sound buzzer if attached
-			PORTB = portsval;
-			_delay_ms(250);
-			#endif
-			return;
+		case 0x10: // Left
+			ReportData->HAT = 0x06;
+			break;
+		case 0x90: // Top-Left
+			ReportData->HAT = 0x07;
+			break;
+		default:
+			ReportData->HAT = 0x08;
 	}
+	
+	//ReportData->LX = xpos;
+	//ReportData->LY = ypos;
+	
+	if (done_printing) return;
+	
+	if (reset)
+	{
+	   reset_count++;
+	   
+	   ReportData->HAT = 0x2;
+	   
+	   if (reset_count >= 800)
+	   {
+	      reset_count = 0;
+	      reset = false;
+	      
+	      xpos = 320;
+	      ypos++;
+	   }
+	   return;
+	}
+	
+	input_brakes++;
+	
+	if (input_brakes >= 5)
+	{
+	   if (reset_request)
+	   {
+	      reset = true;
+	      input_brakes = 0;
+	      reset_request = false;;
+	      return;
+	   }
+	   
+	   meme = !meme;
+	   
+	   if (!meme)
+	      other = !other;
+	      
+	   input_brakes = 0;
+	}
+	
+	if (meme)
+	{
+	   input_count = 0;
+	   
+	   if (xpos >= 0)
+	   {
+	      ReportData->HAT = 0x6; //go left
+	   }
+	   else
+	   {
+	      ReportData->HAT = 0x4;
+	      reset_request = true;
+	   }
+	}
+	else
+	{
+	   if (!input_count)
+	   {
+	      if (xpos >= 0)
+	         xpos--;
+	   }
+	   
+	   if ((image_data[(xpos / 8)+(ypos*40)] & 1 << (xpos % 8)) && (xpos >= 0))
+	      ReportData->Button |= 0x4;
+	      
+	   if (xpos <= 0 && ypos >= 120-1) done_printing = true;
 
-	// Inking
-	if (state != SYNC_CONTROLLER && state != SYNC_POSITION)
-		if (pgm_read_byte(&(image_data[(xpos / 8) + (ypos * 40)])) & 1 << (xpos % 8))
-			ReportData->Button |= SWITCH_A;
-
-	// Prepare to echo this report
-	memcpy(&last_report, ReportData, sizeof(USB_JoystickReport_Input_t));
-	echoes = ECHOES;
-
+	   input_count++;
+   }
 }
