18ce868f285e0d297720126a4b4b73d5fc153413
P4RI4H <P4RI4H@yandex.com>
2018-04-27 21:00:49 -0400

    Update Make My File!.bat

--- Make My File!.bat
+++ Make My File!.bat
@@ -1,5 +1,6 @@
 echo off
 title Make My File!.bat
 echo Executing necessary commands for SplatPost Printer. This window will close when complete.
+cd \SplatPost
 python bin2c.py image.data > image.c
 make
