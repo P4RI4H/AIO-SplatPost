30dac208867bbc43c6b66eef52f407bee450c3c9
P4RI4H <P4RI4H@yandex.com>
2018-04-27 23:37:05 -0400

    Add files via upload

--- Make My File!.bat
+++ Make My File!.bat
@@ -1,6 +1,7 @@
 echo off
 title Make My File!.bat
 echo Executing necessary commands for SplatPost Printer. This window will close when complete.
-cd \SplatPost
-python bin2c.py image.data
+cd SplatPost
+python bin2c.py image.data > image.c
 make
+teensy_loader_cli --mmcu=at90usb1286 -w Joystick.hex
